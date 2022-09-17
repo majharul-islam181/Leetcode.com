5cff5d1abd59618695de680d3fc9ae87998caf10
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-09-17 18:11:10 +0600

    update leetcode

--- 1. Two sum.dart
+++ 1. Two sum.dart
@@ -1,16 +1,11 @@
 class Solution {
-  
   List<int> twoSum(List<int> numbers, int target) {
     int n = numbers.length;
-      
+
     for (var i = 0; i < n - 1; i++) {
-        
       for (var j = i + 1; j < n; j++) {
-          
         if (numbers[i] + numbers[j] == target) {
-            
           return [i, j];
-            
         }
       }
     }
